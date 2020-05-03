d89492e1eca1ad29d01e4f8eb01cb4bfdd268d28
PujaFadte <63729052+PujaFadte@users.noreply.github.com>
2020-05-03 15:23:27 +0200

    Unity project create

---
+++ IntegrationTest/Assets/Scripts/MoveAgent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveAgent : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Move");
+    }
+
+    IEnumerator Move() {
+        while (true) {
+            agent.destination = new Vector3(Random.Range(-5, 5), 0, Random.Range(5,-5));
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+}
